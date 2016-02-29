04d70d654789173dff399fcdf3888afec66a5b32
Martin Crean <martincrean@hotmail.com>
2016-02-29 17:28:19 +0000

    Initial Commit

---
+++ src/MvcMovie/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // GET: /Hello/ 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+    }
+}
